526ef698ca623efae4a19a76925bd2aa75aabb0b
katarzyna <katarzyna.radecka@britenet.com.pl>
2021-10-04 09:02:13 +0200

    small change

--- src/main/kotlin/Payload.kt
+++ src/main/kotlin/Payload.kt
@@ -1,11 +1,20 @@
+import io.restassured.path.json.JsonPath
+
 open class Payload {
+
+    fun rawToJson(resp:String): JsonPath {
+        val js1: JsonPath = JsonPath(resp)
+        return js1
+    }
 
 
     fun addUser(): String {
+        var name = "morpheus"
+        var job = "leader"
 
         return "{\n" +
-                "    \"name\": \"morpheus\",\n" +
-                "    \"job\": \"leader\"\n" +
+                "    \"name\": \""+name+"\",\n" +
+                "    \"job\": \""+job+"\"\n" +
                 "}"
     }
 
@@ -18,5 +27,39 @@ open class Payload {
                 "    \"job\": \""+newJob+"\"\n" +
                 "}"
     }
+
+    fun registerUser(): String{
+        var newEmail = "eve.holt@reqres.in"
+        var newPassword = "pistol"
+
+        return "{\n" +
+                "    \"email\": \""+newEmail+"\",\n" +
+                "    \"password\": \""+newPassword+"\"\n" +
+                "}"
+    }
+
+    fun incorrectData(): String {
+        var newIncorrectEmail = "sydney@fife"
+        return "{\n" +
+                "    \"email\": \""+newIncorrectEmail+"\"\n" +
+                "}"
+    }
+
+    fun loginSuccessful():String{
+        var login = "eve.holt@reqres.in"
+        var password = "cityslicka"
+        return "{\n" +
+                "    \"email\": \""+login+"\",\n" +
+                "    \"password\": \""+password+"\"\n" +
+                "}"
+    }
+
+    fun loginUnuccessful():String{
+        var badLogin = "eve.holt@reqres.in"
+        return "{\n" +
+                "    \"email\": \""+badLogin+"\"\n" +
+                "}"
+    }
+
 
 }
