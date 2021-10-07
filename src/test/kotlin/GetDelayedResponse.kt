beae098f37024b6d6b76934392ce810b37b5b4e0
katarzyna <katarzyna.radecka@britenet.com.pl>
2021-10-07 12:01:11 +0200

    add new tests and fixed payload

--- src/test/kotlin/GetDelayedResponse.kt
+++ src/test/kotlin/GetDelayedResponse.kt
@@ -2,6 +2,8 @@ import data.BaseTest
 import data.Payload
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.greaterThan
 import org.junit.Test
 
 class GetDelayedResponse: BaseTest() {
@@ -12,8 +14,43 @@ class GetDelayedResponse: BaseTest() {
             .queryParam("delay","3")
             .`when`().get("users")
             .then().assertThat().statusCode(200).assertThat()
+            .body("data.size()" , greaterThan(0)).assertThat()
             .contentType(ContentType.JSON)
             .extract().response().asString()
             Payload().log(getDelayedResponse)
+    }
+
+    @Test
+    fun getEmptyPageDelayedResponse(){
+        val getEmptyPageDelayedResponseResponse =  given().spec(requestSpecification)
+            .queryParam("delay","")
+            .`when`().get("users")
+            .then().assertThat().statusCode(200).assertThat()
+            .body("data.size()" , greaterThan(0)).assertThat()
+            .contentType(ContentType.JSON)
+            .extract().response().asString()
+        Payload().log(getEmptyPageDelayedResponseResponse)
+    }
+
+    @Test
+    fun getEmptyDelayedResponse(){
+        val getEmptyDelayedResponseResponse =  given().spec(requestSpecification)
+            .queryParam("delay","8888888888888888888888888888888888888")
+            .`when`().get("users")
+            .then().assertThat().statusCode(200).assertThat()
+            .body("data.size()" , greaterThan(0)).assertThat()
+            .contentType(ContentType.JSON)
+            .extract().response().asString()
+        Payload().log(getEmptyDelayedResponseResponse)
+    }
+
+    @Test
+    fun getToLongDelayedResponse(){
+        val getDelayedResponse =  given().spec(requestSpecification)
+            .queryParam("delay",Payload().veryLongPage())
+            .`when`().get("users")
+            .then().assertThat().statusCode(400)
+            .extract().response().asString()
+        Payload().log(getDelayedResponse)
     }
 }
